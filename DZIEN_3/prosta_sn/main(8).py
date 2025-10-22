561ece6569be49a6296eefad9cffc98a08d34301
albim72 <marcin.albiniak72@gmail.com>
2025-10-22 10:25:00 +0200

    Add files via upload

---
+++ DZIEN_3/prosta_sn/main(8).py
@@ -0,0 +1,24 @@
+import numpy as np
+from simplenn import SimpleNeuralNetwork
+
+
+network = SimpleNeuralNetwork()
+print(network)
+
+#zestaw danych treningowych
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,0],[0,0,0],[0,0,1],[0,1,1]])
+#zestaw etykiet
+train_outputs = np.array([[1,0,1,1,1,1,0,0]]).T
+#liczba epok
+train_iters = 50_000
+
+#trening sieci neuronowej
+network.train(train_inputs,train_outputs,train_iters)
+print(f"wytrenowane wagi:\n {network.weights}")
+
+#budowa zbioru testowego
+test_data = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0]])
+
+print("________ predykcja __________")
+for data in test_data:
+    print(f"wynik dla {data} wynosi: {network.propagation(data)}")
